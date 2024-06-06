192da54d82d4442306a7ef80cd44b34c04e12a86
MuhammedYahiya <yahiyahmhd@gmail.com>
2024-06-07 02:22:35 +0530

    Login Page Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:users_app/authentication/login_screen.dart';
 import 'package:users_app/authentication/signup_screen.dart';
 
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Colors.black,
       ),
-      home: SignUpScreen() ,
+      home: LoginScreen() ,
     );
   }
 }
